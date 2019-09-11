1ab6e3f95b2bfd56846720c0a3a135f3abf3519c
FaustoFe <fausto.2000.22@gmail.com>
2019-09-11 16:21:50 -0300

    Prueba MapaGUI

--- src/Main/MapaGui.java
+++ src/Main/MapaGui.java
@@ -10,31 +10,53 @@ import javax.swing.border.Border;
 public class MapaGui extends JFrame {
 	
 	//Atrubutos
-	protected Container contenPane;
 	protected JPanel gamePanel;
-	protected JLabel lvlBackground;
 
 	public MapaGui() { 
-	   super("TorreDefensa"); 
-	   setBounds(new Rectangle(1366, 768));
-	   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	   Container contenPane = this.getContentPane();
-	   setVisible(true);
-	   gamePanel = new JPanel();
-	   gamePanel.setLayout(new FlowLayout());
-	   gamePanel.setSize(contenPane.getSize());
-	   contenPane.add(gamePanel);
-	   dibujarPanelDeJuego(1);
-	   
+		super("TorreDefensa"); 
+		setBounds(new Rectangle(1366, 768));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		//PRUEBA 1 (INCLUYE LA CLASE DE ABAJO)
+		gamePanel = new fondoNivel();
+		//////////////////////////////////////
+		//PRUEBA 2 (INCLUYE METODO DE ABAJO)
+//		gamePanel = new JPanel();
+		//////////////////////////////////////
+		setContentPane(gamePanel) ;
+//		dibujarPanelDeJuego(1);
+		this.repaint();
 	}
-	
-	public void dibujarPanelDeJuego(int nivel) {
-		ImageIcon fondoNivel = new ImageIcon(this.getClass().getResource("/texturas/nivel1.png"));
-		lvlBackground.setIcon(fondoNivel);
-		lvlBackground.setSize(contenPane.getSize());
-		contenPane.add(lvlBackground);
-//		System.out.println("Ruta del archivo: " + this.getClass().getResource("/texturas/nivel1.png").getPath());
+	//PRUEBA 2
+//	public void dibujarPanelDeJuego(int nivel) {
+//		ImageIcon fondo = new ImageIcon(MapaGui.class.getClassLoader().getResource("texturas/fondo_nivel1.png"));
+//		fondoNivel = new JLabel(fondo);
+////		fondoNivel.setSize(gamePanel.getSize());
+//		gamePanel.add(fondoNivel);
+//		fondoNivel.setVisible(true);
+////		System.out.println("Ruta: " + MapaGui.class.getClassLoader().getResource("texturas/fondo_nivel1.png"));
 		
+//	}
+	/////////////////////////////
+	
+	class fondoNivel extends JPanel {
+	  Image image;
+	  public fondoNivel()
+	  {
+	    try
+	    {
+	      image = javax.imageio.ImageIO.read(new java.net.URL(MapaGui.class.getClassLoader().getResource("texturas/fondo_nivel1.png"),"fondoNivel"));
+	    }
+	    catch (Exception e) { /*handled in paintComponent()*/ }
+	  }
+	 
+	  @Override
+	  protected void paintComponent(Graphics g)
+	  {
+	    super.paintComponent(g);
+	    if (image != null)
+	      g.drawImage(image, 0,0,this.getWidth(),this.getHeight(),this);
+	  }
 	}
 
 }
