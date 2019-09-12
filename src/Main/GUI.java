fc41ae13dc36f5e584361c666610b291f400e972
FaustoFe <fausto.2000.22@gmail.com>
2019-09-12 00:40:07 -0300

    Al fin funciona el fondo :_D

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -3,8 +3,13 @@ package Main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,6 +19,7 @@ import javax.swing.JLabel;
 public class GUI extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel panelGame;
 
 	/**
 	 * Launch the application.
@@ -37,23 +43,80 @@ public class GUI extends JFrame {
 	 */
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(new Rectangle(1366, 768));
+		setBounds(new Rectangle(1280, 720));
+		this.setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setSize(this.getSize());
-//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
 		
-//		JPanel panelJuego = new JPanel();
-//		panelJuego.setBounds(12, 12, 426, 251);
-//		contentPane.add(panelJuego);
+//		panelGame = new JPanel();
+//		panelGame.setSize(contentPane.getSize());
+//		contentPane.add(panelGame);		
+//		panelGame.setLayout(null);
+//		inicializarTienda();
+		
 		
 		JLabel lblFondo = new JLabel();
 		lblFondo.setSize(contentPane.getSize());
-//		lblFondo.setBounds(12, 0, 426, 263);
-		lblFondo.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("texturas/fondo_nivel1.png")));
-		contentPane.add(lblFondo);
-//		System.out.println(GUI.class.getClassLoader().getResource("texturas/fondo_nivel1.png"));
+		lblFondo.setIcon(new ImageIcon(new ImageIcon(this.getClass().getClassLoader().getResource("texturas/fondo_nivel1.png")).getImage().getScaledInstance(lblFondo.getWidth(), lblFondo.getHeight(), Image.SCALE_SMOOTH)));
 		
+		//OTRA OPCION
+//		BufferedImage img = null;
+//		try {
+//		    img = ImageIO.read(new File(this.getClass().getClassLoader().getResource("texturas/fondo_nivel1.png").getPath()));
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
+//		Image dimg = img.getScaledInstance(lblFondo.getWidth(), lblFondo.getHeight(), Image.SCALE_SMOOTH);
+//		ImageIcon imageIcon = new ImageIcon(dimg);
+//		lblFondo.setIcon(imageIcon);
+		
+		contentPane.add(lblFondo);
+		
+	}
+		
+	public void inicializarTienda() {
+		JPanel panelTienda = new JPanel();
+		panelTienda.setBackground(Color.ORANGE);
+		panelTienda.setBounds(12, 519, 1256, 169);
+		panelGame.add(panelTienda);
+		panelTienda.setLayout(null);
+		
+		JLabel lblObj1 = new JLabel("Obj1");
+		lblObj1.setBackground(Color.ORANGE);
+		lblObj1.setBounds(12, 12, 145, 145);
+		panelTienda.add(lblObj1);
+		
+		JLabel lblObj2 = new JLabel("Obj2");
+		lblObj2.setBackground(Color.ORANGE);
+		lblObj2.setBounds(169, 12, 145, 145);
+		panelTienda.add(lblObj2);
+		
+		JLabel lblObj3 = new JLabel("Obj3");
+		lblObj3.setBackground(Color.ORANGE);
+		lblObj3.setBounds(326, 12, 145, 145);
+		panelTienda.add(lblObj3);
+		
+		JLabel lblObj4 = new JLabel("Obj4");
+		lblObj4.setBackground(Color.ORANGE);
+		lblObj4.setBounds(483, 12, 145, 145);
+		panelTienda.add(lblObj4);
+		
+		JLabel lblObj5 = new JLabel("Obj5");
+		lblObj5.setBackground(Color.ORANGE);
+		lblObj5.setBounds(640, 12, 145, 145);
+		panelTienda.add(lblObj5);
+		
+		JLabel lblObj6 = new JLabel("Obj6");
+		lblObj6.setBackground(Color.ORANGE);
+		lblObj6.setBounds(797, 12, 145, 145);
+		panelTienda.add(lblObj6);
+		
+		JLabel lblObj7 = new JLabel("Obj7");
+		lblObj7.setBackground(Color.ORANGE);
+		lblObj7.setBounds(954, 12, 145, 145);
+		panelTienda.add(lblObj7);
 	}
 }
